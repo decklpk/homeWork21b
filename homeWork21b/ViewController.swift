b48554f2cf27024963e013dfb75269f6977c1ca7
Dmitry Khartuev <decklpk@gmail.com>
2021-08-15 19:13:51 +0300

    Initial Commit

---
+++ homeWork21b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork21b
+//
+//  Created by Dmitry K on 15.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
